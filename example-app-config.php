841888a03ee7083150356ebe5635fdefd20ffb18
Andrew Cassell <alc277@gmail.com>
2012-09-05 12:26:08 -0400

    cleaned up config files to represent new config

--- example-app-config.php
+++ example-app-config.php
@@ -1,12 +1,15 @@
 <?php
 
-$GLOBALS['DATABASE_CONFIG']['application'] = new DatabaseConfiguration();
-$GLOBALS['DATABASE_CONFIG']['application']->configureMaster('application', 'localhost', 'mysqluser', 'mysqlpassword');
+$GLOBALS[SQLICIOUS_CONFIG_GLOBAL]['application'] = new DatabaseConfiguration();
+$GLOBALS[SQLICIOUS_CONFIG_GLOBAL]['application']->configureMaster('application', 'localhost', 'mysqluser', 'mysqlpassword');
+$GLOBALS[SQLICIOUS_CONFIG_GLOBAL]['application']->setGeneratorCodeDestinationDirectory('/Library/WebServer/Documents/application/inc/sqlicious/dao/application');
 
-$GLOBALS['DATABASE_CONFIG']['mail'] = new DatabaseConfiguration();
-$GLOBALS['DATABASE_CONFIG']['mail']->configureMaster('mail', 'localhost', 'mysqluser', 'mysqlpassword');
+$GLOBALS[SQLICIOUS_CONFIG_GLOBAL]['mail'] = new DatabaseConfiguration();
+$GLOBALS[SQLICIOUS_CONFIG_GLOBAL]['mail']->configureMaster('mail', 'localhost', 'mysqluser', 'mysqlpassword');
+$GLOBALS[SQLICIOUS_CONFIG_GLOBAL]['mail']->setGeneratorCodeDestinationDirectory('/Library/WebServer/Documents/application/inc/sqlicious/dao/mail');
 
-$GLOBALS['DATABASE_CONFIG']['stats'] = new DatabaseConfiguration();
-$GLOBALS['DATABASE_CONFIG']['stats']->configureMaster('stats', 'localhost', 'mysqluser', 'mysqlpassword');
+$GLOBALS[SQLICIOUS_CONFIG_GLOBAL]['stats'] = new DatabaseConfiguration();
+$GLOBALS[SQLICIOUS_CONFIG_GLOBAL]['stats']->configureMaster('stats', 'localhost', 'mysqluser', 'mysqlpassword');
+$GLOBALS[SQLICIOUS_CONFIG_GLOBAL]['stats']->setGeneratorCodeDestinationDirectory('/Library/WebServer/Documents/application/inc/sqlicious/dao/stats');
 
 ?>
--- example-phpfog-config.php
+++ example-phpfog-config.php
@@ -1,6 +1,6 @@
 <?php
 
-$GLOBALS['DATABASE_CONFIG']['application'] = new DatabaseConfiguration();
-$GLOBALS['DATABASE_CONFIG']['application']->configureMaster($_SERVER['MYSQL_DB_NAME'], $_SERVER['MYSQL_DB_HOST'], $_SERVER['MYSQL_USERNAME'], $_SERVER['MYSQL_PASSWORD']);
+$GLOBALS[SQLICIOUS_CONFIG_GLOBAL]['application'] = new DatabaseConfiguration();
+$GLOBALS[SQLICIOUS_CONFIG_GLOBAL]['application']->configureMaster($_SERVER['MYSQL_DB_NAME'], $_SERVER['MYSQL_DB_HOST'], $_SERVER['MYSQL_USERNAME'], $_SERVER['MYSQL_PASSWORD']);
 
 ?>
