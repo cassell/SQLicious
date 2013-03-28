b79fa1ff5b7eddd1d0c9aeb0dcc91e3e2bcd337a
Andrew Cassell <alc277@gmail.com>
2013-03-28 00:05:36 -0400

    object creation

--- generator/www/api/table/object_creation.php
+++ generator/www/api/table/object_creation.php
@@ -10,44 +10,8 @@ if($db != null)
 {
 	if($table != null)
 	{
-		/*
-		 * $asset = new {{className}}();
-$asset->setLocationId();
-$asset->setEquipmentTypeLinkId();
-$asset->setEquipmentId();
-$asset->setName();
-$asset->setParentId();
-$asset->setEmail();
-$asset->setUpdateDate();
-$asset->setUpdatedBy();
-$asset->save();
-
-// non extened object
-$asset = new {{className}}DaoObject();
-		 */
-		
 		$resp['html'] = $db->getObjectCreationCode($table);
 		returnResponse($resp);
-		
-//		$resp['className'] = ucfirst(SQLiciousGenerator::toFieldCase($table));
-//		$resp['variableName'] = SQLiciousGenerator::toFieldCase($table);
-//			
-//		$columns = $db->getColumns($table);
-//		
-//		if($columns != null && count($columns) > 0)
-//		{
-//			foreach($columns as $column)
-//			{
-//				$c = array();
-//				if($column['Key'] != "PRI")
-//				{
-//					$resp['columns'][] = array('name' => $column['Field'], 'setter' => 'set' . ucfirst(SQLiciousGenerator::toFieldCase($column['Field'])));
-//				}
-//			}
-//		}
-//		
-//		
-		
 	}
 	else
 	{
