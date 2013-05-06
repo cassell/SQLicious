379edc9af18db95bfe48fcfa2ba810499f2525fe
Andrew Cassell <alc277@gmail.com>
2013-05-06 10:42:27 -0400

    cleanup

--- example-phpfog-config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$GLOBALS[SQLICIOUS_CONFIG_GLOBAL] = new SQLiciousConfig();
-
-$GLOBALS[SQLICIOUS_CONFIG_GLOBAL]['application'] = new DatabaseConfiguration('application');
-$GLOBALS[SQLICIOUS_CONFIG_GLOBAL]['application']->configureMaster($_SERVER['MYSQL_DB_NAME'], $_SERVER['MYSQL_DB_HOST'], $_SERVER['MYSQL_USERNAME'], $_SERVER['MYSQL_PASSWORD']);
-$GLOBALS[SQLICIOUS_CONFIG_GLOBAL]['application']->setGeneratorCodeDestinationDirectory('/Library/WebServer/Documents/application/inc/sqlicious/dao/application'); // this is only used in development
-
-?>
